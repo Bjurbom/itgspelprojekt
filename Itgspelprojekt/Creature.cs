c7418a69b9c017a50c378b4d5436c96cf03d3200
Tommie55555 <tommie.van.klaveren16@itgksd.se>
2018-02-21 15:27:26 +0100

    Added MonoGame.Extended, MonoGame Extended Pipeline, and MonoGame Extended Tiled

---
+++ Itgspelprojekt/Creature.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Itgspelprojekt
+{
+    class Creature
+    {
+        float position, walkSpeed, runSpeed;
+
+        Enum MovementDirection { up, down, left, right };
+    }
+}
