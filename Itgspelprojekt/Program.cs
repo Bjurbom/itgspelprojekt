ec289ef41a5d86a92a53f5ae500b3c729efc7d04
Tommie55555 <tommie.van.klaveren16@itgksd.se>
2018-02-21 15:12:37 +0100

    added secret message <3

--- Itgspelprojekt/Program.cs
+++ Itgspelprojekt/Program.cs
@@ -14,7 +14,7 @@ namespace Itgspelprojekt
         [STAThread]
         static void Main()
         {
-            using (var game = new Game1())
+            using (var game = new Game1()) //lolkys
                 game.Run();
         }
     }
