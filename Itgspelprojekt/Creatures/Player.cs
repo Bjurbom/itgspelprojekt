1c39a25d9c9a5c02e8e403cd8de16f9b8df8f91d
Tor Bjurbom <34156187+Bjurbom@users.noreply.github.com>
2018-03-07 12:02:01 +0100

    map update

---
+++ Itgspelprojekt/Creatures/Player.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Itgspelprojekt.Creatures
+{
+    class Player : Creature
+    {
+        public Player(string name, Vector2 position, float moveSpeed, Texture2D texture) : base(name, position, moveSpeed, texture)
+        {
+
+        }
+
+        public void PlayerUpdate()
+        {
+            // up
+            if (Keyboard.GetState().IsKeyDown(Keys.W))
+            {
+                targetPosition.Y = position.Y + (float)64;
+            }
+            // down
+            if (Keyboard.GetState().IsKeyDown(Keys.S))
+            {
+
+            }
+            // right 
+            if (Keyboard.GetState().IsKeyDown(Keys.D))
+            {
+
+            }
+            // left
+            if (Keyboard.GetState().IsKeyDown(Keys.A))
+            {
+
+            }
+        }
+    }
+}
