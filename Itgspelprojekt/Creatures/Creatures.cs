604ca5e27c8806a124d9934951d70e9f2043bf53
Tommie55555 <tommie.van.klaveren16@itgksd.se>
2018-03-08 12:15:50 +0100

    Added Creatures class, and Creatures.xml

---
+++ Itgspelprojekt/Creatures/Creatures.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Itgspelprojekt.Creatures
+{
+    class Creatures
+    {
+
+        public void ReadCreaturesTXT(string creaturesFile)
+        {
+            creaturesFile = "content/creatures.xnb";
+            int counter = 0;
+            string line;
+
+            // Read the file and display it line by line.  
+            System.IO.StreamReader file =
+                new System.IO.StreamReader(creaturesFile);
+            while ((line = file.ReadLine()) != null)
+            {
+
+
+
+            }
+
+            file.Close();
+        }
+    }
+}
